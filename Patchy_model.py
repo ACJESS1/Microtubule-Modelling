7accc4e4ef24a91b1f7e8f774d1084341a3aad54
ale <alexander.smulian@yahoo.com>
2023-08-01 20:59:38 +0100

    All Sites are now circular patches

--- Patchy_model.py
+++ Patchy_model.py
@@ -14,17 +14,20 @@ import numpy
 import gsd.hoomd
 import sphere_fres as sf
 
-N_particles = 5
+N_particles = 25
 
-L = 5
+L = 7
 position=[]
-for i in range(N_particles):
-   position.append([i*1-2.5,0,0])
+for i in range(int(N_particles/5)):
+   position.append([[i-2,-2,0],[i-2,-1,0],[i-2,0,0],[i-2,1,0],[i-2,2,0]])
 
-asdf=math.sqrt(2)/2
+angs=math.sin(math.pi*(1/4))
+angc=math.cos(math.pi*(1/4))
 
 orientation = [(1, 0, 0, 0)] * N_particles
-#orientation = [(asdf, 0, asdf, 0)] * N_particles
+#orientation = [(angc, 0, 0, angs)] * N_particles
+#orientation = [(angc,angs, 0,  0)] * N_particles
+#orientation = [(angc, 0, 0, angs),(1,0,0,0)] 
 #orientation = [(0, 0, 1, 0)] * N_particles
 
 
@@ -46,17 +49,19 @@ sim.create_state_from_gsd(filename='initial.gsd')
 
 kf_delta_deg = 10  # half-opening angle of patches
 kf_epsilon = 1.0  # strength of patchy interaction in kT
-kf_lambda = 3  # range of patchy interaction
+kf_lambda = 2.0  # range of patchy interaction
 sigma = 1.0  # hard core diameter
 
-#E_long site parameters
-a_long = 1
-b_long = 5
-r_long = 1
+q_angle = [math.cos(math.pi*(1/4)), 0, 0, math.sin(math.pi*(1/4))]
 
-a_lat = 1
-b_lat = 3
-r_lat = 1.5
+#E_long site parameters
+a_long = 4.5
+b_long = 10
+r_long = 0.065
+
+a_lat = 6.3
+b_lat = 4.7
+r_lat = 0.04
 
 
 mc = hoomd.hpmc.integrate.Sphere(default_d=0.05, default_a=0.1)
@@ -81,9 +86,15 @@ const float r_a = {r_lat};
 const float kT = param_array[0];
 const float beta_epsilon = epsilon/kT;
 
+const const vec3<float> temp(0, 0, sqrtf(2)/2);
+const quat<float> q_perp(sqrtf(2)/2,temp);
+
 const vec3<float> ehat_particle_reference_frame(1, 0, 0);
 vec3<float> ehat_i = rotate(q_i, ehat_particle_reference_frame);
 vec3<float> ehat_j = rotate(q_j, ehat_particle_reference_frame);
+
+vec3<float> ehat_i_p = rotate(q_perp*q_i, ehat_particle_reference_frame);
+vec3<float> ehat_j_p = rotate(q_perp*q_j, ehat_particle_reference_frame);
 
 vec3<float> r_hat_ij = r_ij / sqrtf(dot(r_ij, r_ij));
 bool patch_on_i_is_aligned_with_r_ij = dot(ehat_i, r_hat_ij) >= cos(delta);
@@ -91,24 +102,25 @@ bool patch_on_j_is_aligned_with_r_ji = dot(ehat_j, -r_hat_ij) >= cos(delta);
 bool patch_on_i_is_aligned_with_r_ji = dot(ehat_i, -r_hat_ij) >= cos(delta);
 bool patch_on_j_is_aligned_with_r_ij = dot(ehat_j, r_hat_ij) >= cos(delta);
 
-bool patch_on_i_is_perpendicular_r_ij = dot(ehat_i, r_hat_ij) >= cos(delta-M_PI/2);
-bool patch_on_j_is_perpendiculat_r_ij = dot(ehat_j, r_hat_ij) >= cos(delta-M_PI/2);
+bool patch_on_i_is_perpendicular_r_ij = abs(dot(ehat_i_p, r_hat_ij)) >= cos(delta);
+bool patch_on_j_is_perpendiculat_r_ij = abs(dot(ehat_j_p, r_hat_ij)) >= cos(delta);
 
 float rsq = dot(r_ij, r_ij);
 float r_ij_length = sqrtf(rsq);
+float r_ij_ls = r_ij_length - sigma;
 if ((patch_on_i_is_aligned_with_r_ij || patch_on_i_is_aligned_with_r_ji)
     && (patch_on_j_is_aligned_with_r_ji || patch_on_j_is_aligned_with_r_ij)
     && r_ij_length < lambda*sigma)
     {{
-    return ((a_o* pow(r_ij_length/r_o,2)*exp(-r_ij_length/r_o))
-        -b_o*exp(-(pow(r_ij_length/r_o,2))) );
+    return ((a_o* pow(r_ij_ls/r_o,2)*exp(-r_ij_ls/r_o))
+        -b_o*exp(-(pow(r_ij_ls/r_o,2))) );
     }}
 else if (patch_on_i_is_perpendicular_r_ij
          && patch_on_j_is_perpendiculat_r_ij
          && r_ij_length < lambda*sigma)
     {{
-    return ((a_a* pow(r_ij_length/r_a,2)*exp(-r_ij_length/r_a))
-    -b_a*exp(-(pow(r_ij_length/r_a,2))) );      
+    return ((a_a* pow(r_ij_ls/r_a,2)*exp(-r_ij_ls/r_a))
+    -b_a*exp(-(pow(r_ij_ls/r_a,2))) );      
     }}
 else
     {{
@@ -127,12 +139,12 @@ patch_potential = hoomd.hpmc.pair.user.CPPPotential(r_cut=r_cut,
                                                     param_array=[initial_kT])
 
 mc.pair_potential = patch_potential
-sim.run(10000)
+sim.run(0000)
 
 with sim.state.cpu_local_snapshot as sn:
-    print(sn.particles.typeid)
-    print(sn.particles.position)
-    print(sn.particles.orientation)
+    #print(sn.particles.typeid)
+    #print(sn.particles.position)
+    #print(sn.particles.orientation)
     dist =sn.particles.position[0,:]- sn.particles.position[1,:]
-    print(math.sqrt(numpy.dot(dist, dist)))
+    #print(math.sqrt(numpy.dot(dist, dist)))
 print(patch_potential.energy)
